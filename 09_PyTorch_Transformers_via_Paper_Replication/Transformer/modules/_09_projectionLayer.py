e913723459b36bf1cb818da038fed01988c80e4c
Gaurav Mishra <gauravmishraokok@gmail.com>
2024-09-21 19:01:32 +0530

    Projection Layer

--- 09_PyTorch_Transformers_via_Paper_Replication/Transformer/modules/_09_projectionLayer.py
+++ 09_PyTorch_Transformers_via_Paper_Replication/Transformer/modules/_09_projectionLayer.py
@@ -1,12 +1,35 @@
 import torch
 import torch.nn as nn
+
 class ProjectionLayer(nn.Module):
+    """
+    A projection layer that projects the input tensor to the vocabulary size and applies log softmax.
+
+    Attributes:
+        proj (nn.Linear): Linear layer to project the input tensor to the vocabulary size.
+    """
     
-    def __init__(self , d_model:int , vocab_size : int):
+    def __init__(self, d_model: int, vocab_size: int):
+        """
+        Initializes the ProjectionLayer.
+
+        Args:
+            d_model (int): The dimension of the input tensor.
+            vocab_size (int): The size of the vocabulary.
+
+        """
         super().__init__()
-        
         self.proj = nn.Linear(in_features=d_model, out_features=vocab_size)
         
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the projection layer.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (Batch, Seq_len, d_model).
+
+        Returns:
+            torch.Tensor: Output tensor of shape (Batch, Seq_len, vocab_size) after applying log softmax.
+        """
         return torch.log_softmax(self.proj(x), dim=-1)
-        #Dim -1 is the last dimension, Log Softmax is used for numerical stability
+        # Dim -1 is the last dimension, Log Softmax is used for numerical stability
