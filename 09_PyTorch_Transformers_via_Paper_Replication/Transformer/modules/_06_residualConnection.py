275e06cf44552af90ccdd00361195f9c6ca87c95
Gaurav Mishra <gauravmishraokok@gmail.com>
2024-09-21 19:01:07 +0530

    Residual Connection

--- 09_PyTorch_Transformers_via_Paper_Replication/Transformer/modules/_06_residualConnection.py
+++ 09_PyTorch_Transformers_via_Paper_Replication/Transformer/modules/_06_residualConnection.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import math
@@ -6,16 +5,34 @@ from modules._03_layerNormalisation import LayerNormalization
 
 class ResidualConnection(nn.Module):
     """
-    Makes a residual connection
+    Implements a residual connection followed by layer normalization.
 
-    Args:
-        dropout (float): The dropout rate in the Dropout layer.
+    Attributes:
+        dropout (nn.Dropout): Dropout layer.
+        norm (LayerNormalization): Layer normalization.
     """
-    def __init__(self,dropout):
-        super().__init__()
-        
+    
+    def __init__(self, dropout: float, features: int):
+        """
+        Initializes the ResidualConnection module.
+
+        Args:
+            dropout (float): The dropout rate to be applied.
+            features (int): The number of features for layer normalization.
+        """
+        super(ResidualConnection, self).__init__()
         self.dropout = nn.Dropout(dropout)
-        self.norm = LayerNormalization()
+        self.norm = LayerNormalization(features)
         
-    def forward(self, x, sublayer):
-        return self.dropout(sublayer(self.norm(x))) + x
+    def forward(self, x: torch.Tensor, sublayer: nn.Module) -> torch.Tensor:
+        """
+        Forward pass for the residual connection.
+
+        Args:
+            x (torch.Tensor): Input tensor of shape (Batch, Seq_len, Features).
+            sublayer (nn.Module): The sublayer to apply the residual connection to.
+
+        Returns:
+            torch.Tensor: Output tensor after applying the residual connection and layer normalization.
+        """
+        return x + self.dropout(sublayer(self.norm(x)))
